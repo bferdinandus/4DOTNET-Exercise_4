00b82592c5ce6846852302a9d7809d5d11feb188
Ben Ferdinandus <4364969+bferdinandus@users.noreply.github.com>
2020-10-28 11:46:15 +0100

    Asynchroon "MiddelEeuwen" Framework 4 (2010)

--- WinCalc/Form1.cs
+++ WinCalc/Form1.cs
@@ -25,24 +25,47 @@ namespace WinCalc
             decimal a = getalAUpDown.Value;
             decimal b = getalBUpDown.Value;
 
-            _timeRemaining = 10;
+            _timeRemaining = 4;
             timeRemainingLabel.Text = _timeRemaining.ToString();
             timer1.Start();
 
             var ctx = SynchronizationContext.Current;
+            /*
+            // een delegate kan je asynchroon starten dmv BeginInvoke
             Func<decimal, decimal, decimal> add = Add;
+            IAsyncResult ar = add.BeginInvoke(a, b, ar2 =>
+            {
+                ctx.Post(UpdateAnswer, add.EndInvoke(ar2));
+            }, null);*/
+                        
+            var x = Task.Run(() =>
+            {
+                return Add(a, b);
+            }).ContinueWith(pt =>
+            {
+                /*if (pt.Exception != null)
+                {
+                    Console.WriteLine(pt.Exception.InnerException);
+                }*/
+                ctx.Post(UpdateAnswer, pt.Result);
+            });
 
-            IAsyncResult ar = add.BeginInvoke(a, b, ar2 => ctx.Post(UpdateAnswer, add.EndInvoke(ar2)), null);
+            
         }
 
         private void UpdateAnswer(object result)
         {
+            /*if (answerLabel.InvokeRequired)
+            {
+                answerLabel.Invoke((MethodInvoker)delegate { UpdateAnswer(result); });
+            }*/
+
             answerLabel.Text = result.ToString();
         }
 
         private decimal Add(decimal a, decimal b)
         {
-            Task.Delay(_timeRemaining * 1000).Wait();
+            Task.Delay(_timeRemaining * 1000).Wait(); // long blocking action
 
             return a + b;
         }
